84db9ee8a33b8a8f303ee604e3f374a8a1b82693
cbeuchatmmi <corentin.beuchat@edu.univ-fcomte.fr>
2025-01-28 13:54:34 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aide Asso',
+      title: 'Aide Asso test',
       theme: ThemeData(
         // This is the theme of your application.
         //
